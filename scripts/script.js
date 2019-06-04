b71f0adc7cba940237c6a57598c11321b4349de0
Fabrício Devinar <fc_devinar@hotmail.com>
2019-06-04 18:37:07 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,11 @@
-var list = document.createElement('ul');
-var info = document.createElement('p');
-var html = document.querySelector('html');
+function createParagraph() {
+  let main = document.createElement('main');
+  main.textContent = 'You clicked the button!';
+  document.body.appendChild(main);
+}
 
-info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+const buttons = document.querySelectorAll('button');
 
-document.body.appendChild(info);
-document.body.appendChild(list);
-
-html.onclick = function() {
-  var listItem = document.createElement('li');
-  var listContent = prompt('What content do you want the list item to have?');
-  listItem.textContent = listContent;
-  list.appendChild(listItem);
-
-  listItem.onclick = function(e) {
-    e.stopPropagation();
-    var listContent = prompt('Enter new content for your list item');
-    this.textContent = listContent;
-  }
+for(let i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', createParagraph);
 }
