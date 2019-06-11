9be203693b10f5fe284076b10b4d916770e9a45a
Fabrício Devinar <fc_devinar@hotmail.com>
2019-06-11 19:25:52 -0300

    Add files via upload

--- scripts/jogador_script.js
+++ scripts/jogador_script.js
@@ -1,4 +1,5 @@
-function addPlayer(gol, i) {
+/*
+function addPlayers(gol, i) {
     let main = document.createElement('main');
     main.textContent = gol[i].value;//gol_value;
     console.log(main);
@@ -8,13 +9,24 @@ function addPlayer(gol, i) {
 const buttons = document.querySelectorAll('button');
 //console.log(button);
 
-let gol = document.getElementsByClassName('goleiro');
-console.log(gol);
-let gol_value = gol[0].value;
-//console.log(gol_value);
+//let gol = document.getElementsByClassName('goleiro');
+//console.log(gol);
+//let gol_value = gol[0].value;
+//console.log(gol_value
 
-for(let i = 0; i < buttons.length ; i++) {
-buttons[i].addEventListener('click', addPlayer(gol, i));
+let joga = document.getElementsByClassName('jogador');
+console.log(joga);
+//let gol_value = gol[0].value;
+
+//for(let i = 0; i < buttons.length ; i++) {
+//buttons[i].addEventListener('click', addPlayers(gol, i));
+//}
+*/
+
+function addPlayer(value) {
+  let main = document.createElement('main');
+  main.textContent = value;
+  document.body.appendChild(main);
 }
 
 
