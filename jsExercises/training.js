e68f38bdb0372d61bbd5bd222bfc76eb42d2203e
fdevinar <fc_devinar@hotmail.com>
2019-11-12 21:09:49 -0300

    ‘changes’

--- jsExercises/training.js
+++ jsExercises/training.js
@@ -1,2 +1,34 @@
-var age = prompt('What is your age?');
-alert('You are ' + age * 365 + ' days old!');
+//print all numbers between -10 and 19
+// var count = -10;
+// while (count < 20) {
+//     console.log(count);
+//     count++;
+// }
+
+// print all even numbers between 10 and 40
+// var count = 10;
+// while (count < 41){
+//     if (count %2 === 0){
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+
+// print all odd numbers between 300 and 333
+// var count = 300;
+// while (count < 334){
+//     if (count %2 === 1){
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+// print all numbers divisible by 5 AND 3 between 5 and 50
+// var count = 5;
+// while (count < 51){
+//     if (count %3 === 0 && count %5 ===0){
+//         console.log(count);
+//     }
+//     count++;
+// }
