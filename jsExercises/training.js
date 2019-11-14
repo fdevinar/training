108863b64c9ee2066983eeef89fe86c804281cbe
fdevinar <fc_devinar@hotmail.com>
2019-11-14 18:46:18 -0300

    ‘changes’

--- jsExercises/training.js
+++ jsExercises/training.js
@@ -1,10 +1,23 @@
-var word = prompt('There yet?');
+// print all numbers between -10 and 19
+// for (i=-10; i<20; i++){
+//     console.log(i);
+// }
 
-while (word.indexOf('yes') === -1){
-    var word = prompt('There yet?');
-}
+// print all even numbers between 10 and 40
+// for (i=10; i<41; i+=2){
+//     console.log(i);
+// }
 
-// true && false => false
-// true || false => true
+// print all odd numbers between 300 and 333
+// for (i=300; i<334; i++){
+//     if (i % 2 !== 0){
+//         console.log(i);
+//     }
+// }
 
-alert ("Yay! We finally made it!");
+// print all numbers divisibly by 5 and 3 between 5 and 50
+// for (i=5; i<51; i++){
+//     if (i % 3 === 0 && i % 5 ===0){
+//         console.log(i);
+//     }
+// }
