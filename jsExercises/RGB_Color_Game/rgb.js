3cf9efb39edd6edfcb808491de6aa0c31a04a059
fdevinar <fc_devinar@hotmail.com>
2020-01-25 20:51:35 -0300

    ‘changes’

---
+++ jsExercises/RGB_Color_Game/rgb.js
@@ -0,0 +1,12 @@
+var colors = document.querySelectorAll('.color');
+
+for (i=0; i<colors.length; i++){
+    generateRandomColor(colors[i]);
+}
+
+function generateRandomColor(block){
+    block.style.background = 'yellow';
+
+    // CREATE LOGIN TO GENERATE RANDOM RGB
+
+}
