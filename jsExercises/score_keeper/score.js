d49f7e246f793c0711052c7ea5425c347cb3b35c
fdevinar <fc_devinar@hotmail.com>
2020-01-25 19:22:42 -0300

    ‘changes’

--- jsExercises/score_keeper/score.js
+++ jsExercises/score_keeper/score.js
@@ -1,68 +1,3 @@
-// var movies = {
-//     movieList: [
-//         {
-//             title: "Scarface",
-//             rating: 4.5,
-//             hasWatched: true
-//                 },
-//         {
-//             title: "Godfather",
-//             rating: 5.0,
-//             hasWatched: true
-//         },
-//         {
-//             title: "Interstellar",
-//             rating: 4.0,
-//             hasWatched: false
-//         },
-//         {
-//             title: "Monty Python",
-//             rating: 4.2,
-//             hasWatched: true
-//         },
-//         {
-//             title: "City of God",
-//             rating: 5.0,
-//             hasWatched: true,
-//         }
-//             ],
-//         printList: function(){
-//             this.movieList.forEach(function(film){
-//                 if (film.hasWatched){
-//                     console.log("You have watched \"" + film.title + '\" - ' + film.rating + ' stars');
-//                 }
-//                 else{
-//                     console.log("You have not seen \"" + film.title + '\" - ' + film.rating + ' stars');
-//                 }
-//                                                 });
-//                             }
-//             }
-
-
-// // MANIPULANDO LIST ITEMS
-// var li = document.querySelectorAll('li');
-// console.log(li);
-// li.forEach(function(el){
-//     el.style.color = 'black';
-// })
-
-// MANIPULANDO HINO (p com id)
-// var hino = document.querySelector('#hino');
-
-// hino.style.backgroundColor = 'green';
-// hino.style.borderColor = 'black';
-// hino.style.borderStyle = 'solid';
-
-// hino.innerHTML = hino.innerHTML + ' Para o que der e vier!';
-
-// TOGGLE DARK 
-
-// var body = document.querySelector('body');
-
-// document.querySelector('button').addEventListener("click",function toggleDarkMode(){
-//         body.classList.toggle('dark');
-//     });
-
 
 //*************** SCORE KEEPER GAME**************//
 
@@ -182,3 +117,73 @@ for (i=0; i<lis.length; i++){
         this.classList.remove('lisChange');
     });
 }
+
+
+
+
+// OBJECTS AND DOM MANIPULATION EXERCISES
+
+// var movies = {
+//     movieList: [
+//         {
+//             title: "Scarface",
+//             rating: 4.5,
+//             hasWatched: true
+//                 },
+//         {
+//             title: "Godfather",
+//             rating: 5.0,
+//             hasWatched: true
+//         },
+//         {
+//             title: "Interstellar",
+//             rating: 4.0,
+//             hasWatched: false
+//         },
+//         {
+//             title: "Monty Python",
+//             rating: 4.2,
+//             hasWatched: true
+//         },
+//         {
+//             title: "City of God",
+//             rating: 5.0,
+//             hasWatched: true,
+//         }
+//             ],
+//         printList: function(){
+//             this.movieList.forEach(function(film){
+//                 if (film.hasWatched){
+//                     console.log("You have watched \"" + film.title + '\" - ' + film.rating + ' stars');
+//                 }
+//                 else{
+//                     console.log("You have not seen \"" + film.title + '\" - ' + film.rating + ' stars');
+//                 }
+//                                                 });
+//                             }
+//             }
+
+
+// // MANIPULANDO LIST ITEMS
+// var li = document.querySelectorAll('li');
+// console.log(li);
+// li.forEach(function(el){
+//     el.style.color = 'black';
+// })
+
+// MANIPULANDO HINO (p com id)
+// var hino = document.querySelector('#hino');
+
+// hino.style.backgroundColor = 'green';
+// hino.style.borderColor = 'black';
+// hino.style.borderStyle = 'solid';
+
+// hino.innerHTML = hino.innerHTML + ' Para o que der e vier!';
+
+// TOGGLE DARK 
+
+// var body = document.querySelector('body');
+
+// document.querySelector('button').addEventListener("click",function toggleDarkMode(){
+//         body.classList.toggle('dark');
+//     });
