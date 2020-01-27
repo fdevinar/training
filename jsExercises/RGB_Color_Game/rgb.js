dde36d45c94ba80da75e7b91a95e9d8a580d1803
fdevinar <fc_devinar@hotmail.com>
2020-01-27 19:28:31 -0300

    ‘changes’

--- jsExercises/RGB_Color_Game/rgb.js
+++ jsExercises/RGB_Color_Game/rgb.js
@@ -1,18 +1,30 @@
 var colors = document.querySelectorAll('.color');
 
-for (i=0; i<colors.length; i++){
-    var rgb = generateRandomColor(colors[i]);
-    colors[i].style.background = rgb;
-    colors[i].textContent = rgb;   
-}
-
-
-// console.log(generateRandomColor(colors[0]));
-
+// GENERATE RANDOM COLOR
 function generateRandomColor(block){
     var red = Math.floor(Math.random()*255);
     var green = Math.floor(Math.random()*255);
     var blue = Math.floor(Math.random()*255);
     return ('rgb(' + red + ',' + green + ',' + blue + ')');
 }
+
+// APPLY RANDOM COLOR TO BLOCK
+for (i=0; i<colors.length; i++){
+    var rgb = generateRandomColor(colors[i]);
+    colors[i].style.background = rgb;
+    colors[i].textContent = rgb;   
+};
+
+// APPLY HIGHLIGHT TO BLOCKS WHEN MOUSE OVER
+for (i=0; i<colors.length; i++){
+    colors[i].addEventListener('mouseover',function(){
+        this.classList.add('highlight');
+    })
+};
+// REMOVE HIGHLIGHT FROM BLOCKS WHEN MOUSE OUT
+for (i=0; i<colors.length; i++){
+    colors[i].addEventListener('mouseout',function(){
+        this.classList.remove('highlight');
+    })
+};
 
