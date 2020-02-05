ad59b24db7c63a82b8f39d5d9022623f367ef1a7
fdevinar <fc_devinar@hotmail.com>
2020-02-05 21:38:03 +0530

    ‘changes’

--- jsExercises/RGB_Color_Game/rgb.js
+++ jsExercises/RGB_Color_Game/rgb.js
@@ -8,6 +8,81 @@ function generateRandomColor(block){
     return ('rgb(' + red + ',' + green + ',' + blue + ')');
 }
 
+///////////////////////////////////////////////////////////////////////////
+
+// CHOOSE DIFFICULTY
+
+var diff = 'hard';
+// grabRGB();
+randomRgb = grabRGB();
+
+var level = document.getElementsByClassName('level');
+
+// HIGHLIGHT HARD AS DEFAULT DIFFICULTY
+level[1].classList.add('chosen-diff');
+
+/////// FIX THIS - randomRGB NOT CHANGING AFTER CHOSEN DIFF
+
+// CHANGE DIFF CLICKING ON LEVEL
+for (i=0; i<2; i++){
+    level[i].addEventListener('click',function(){
+        diff = this.getAttribute('value');
+        this.classList.add('chosen-diff');
+        // CHANGE HIGHLIGHT BASED ON LEVEL SELECTED
+        if (diff === 'hard'){
+            document.getElementById('easy').classList.remove('chosen-diff');
+            // showOnHard();
+            grabRGB();
+            console.log(randomRgb);
+        }
+        if (diff === 'easy'){
+            document.getElementById('hard').classList.remove('chosen-diff');
+            hideOnEasy();
+            grabRGB();
+            console.log(randomRgb);
+        }  
+        })
+                }      
+
+// REMOVE BLOCKS BASED ON DIFF
+
+function hideOnEasy(){
+    for (i=3; i<6; i++){
+        colors[i].setAttribute('hidden','true');
+    }
+}
+
+// FIX THIS - CONFIGURE SHOW ON HARD
+
+// function showOnHard(){
+//     for (i=3; i<6; i++){
+//         colors[i].setAttribute('hidden','true');
+//     }
+// }
+
+///////////////////////////////////////////////////////////////////////////
+
+// SET RGB COLOR TO BE GUESSED
+// Grab H2 Element for guessed RGB
+var h2rgb = document.getElementById('guessThisRgb');
+// Grab RGB to be guessed, 0 to 5 - index for color blocks
+function grabRGB(){
+    if (diff === 'hard'){
+        console.log('LOOP HARD');
+        return Math.floor(Math.random() * colors.length);
+    }
+    if (diff === 'easy'){
+        // 
+        console.log('LOOP EASY');
+        return (Math.floor(Math.random() * colors.length) -3);
+    }
+// Prints on screen the RGB to be guessed
+h2rgb.textContent = colors[randomRgb].textContent;
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+
 // APPLY RANDOM COLOR TO BLOCK
 for (i=0; i<colors.length; i++){
     var rgb = generateRandomColor(colors[i]);
@@ -28,14 +103,6 @@ for (i=0; i<colors.length; i++){
         this.classList.remove('highlight');
     })
 };
-
-// SET RGB COLOR TO BE GUESSED
-// Grab H2 Element for guessed RGB
-var h2rgb = document.getElementById('guessThisRgb');
-// Grab RGB to be guessed, 0 to 5 - index for color blocks 
-var randomRgb = Math.floor(Math.random() * colors.length);
-// Prints on screen the RGB to be guessed
-h2rgb.textContent = colors[randomRgb].textContent;
 
 
 // CHECK IF GUESS IS RIGHT
