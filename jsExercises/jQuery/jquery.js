ae98deeb24ab050702197483d5e24c100a1f8b58
fdevinar <fc_devinar@hotmail.com>
2020-02-17 20:24:08 +0530

    ‘changes’

---
+++ jsExercises/jQuery/jquery.js
@@ -0,0 +1,11 @@
+$('li').css({
+    backgroundColor: 'navy',
+    color: 'cyan'
+});
+
+let mcdonalds = {
+    'background-color': 'yellow',
+    'color': 'red',
+    'border': '2px solid black'
+};
+
