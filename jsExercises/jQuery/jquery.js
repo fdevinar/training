df09996fda6bde90754ff3d41779be5291c697b8
fdevinar <fc_devinar@hotmail.com>
2020-02-18 21:02:05 +0530

    ‘changes’

--- jsExercises/jQuery/jquery.js
+++ jsExercises/jQuery/jquery.js
@@ -1,11 +1,18 @@
-$('li').css({
-    backgroundColor: 'navy',
-    color: 'cyan'
-});
+// $('li').css({
+//     backgroundColor: 'navy',
+//     color: 'cyan'
+// });
 
-let mcdonalds = {
-    'background-color': 'yellow',
-    'color': 'red',
-    'border': '2px solid black'
-};
+// let mcdonalds = {
+//     'background-color': 'yellow',
+//     'color': 'red',
+//     'border': '2px solid black'
+// };
 
+
+// $('div').css('background','purple');
+// $('.highlight').css('width','200px');
+// $('#third').css('border','3px solid orange');
+// $('div:first').css('color','cyan');
+
+// $('li').html('<h2>GRITA</h2>')
