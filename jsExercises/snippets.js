daba3af249cfecfc6de1f3c48545af0a520cc790
fdevinar <fc_devinar@hotmail.com>
2020-03-07 00:15:08 +0530

    ‘changes’

---
+++ jsExercises/snippets.js
@@ -0,0 +1,25 @@
+// BETTER COMMENTS:
+
+// TODO SAMPLE TO-DO
+// * IMPORTANT
+// ! ERROR
+// ? QUESTION
+// // STRIKETHROUGH
+
+// JQUERY SAMPLES
+
+
+$('element')
+
+.css('width','200px)
+
+.click(action)
+
+.text('Hello')
+.html('<h1>HI</h1>')
+
+.addClass
+.removeClass
+.toggleClass
+
+
