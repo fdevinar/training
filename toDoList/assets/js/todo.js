a1c7b028c8e5436e89367f4c2efdb4810344fa2d
fdevinar <fc_devinar@hotmail.com>
2020-03-17 20:43:23 +0530

    ‘changes’

--- toDoList/assets/js/todo.js
+++ toDoList/assets/js/todo.js
@@ -1,2 +1,26 @@
 // alert('CONNECTED!');
 
+let list = $('ul');
+let input = $('input');
+
+input.keypress(function(e){
+    console.log(input.val());
+    if (e.keyCode === 13){
+        list.append('<li>' + input.val() + '</li>');
+        input.val('');
+    }
+});
+
+
+
+
+
+
+// let input = $('input[type="text"]');
+// input.keypress(function(e){
+//     console.log(input.val());
+//     // keyCode 13 is pressing Enter
+//     if (e.keyCode === 13){
+//         alert(input.val());
+//     }
+// });
