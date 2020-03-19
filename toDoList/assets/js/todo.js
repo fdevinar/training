d0cbdb99758dd36c6f775a677e23f1b012d91247
fdevinar <fc_devinar@hotmail.com>
2020-03-19 18:18:55 +0530

    ‘changes’

--- toDoList/assets/js/todo.js
+++ toDoList/assets/js/todo.js
@@ -2,15 +2,23 @@
 // GRAB HTML ELEMENTS
 let list = $('ul');
 let input = $('input');
+let plus = $('i');
+let listItem = $('li');
 
-// WRITE LI ON INPUT ENTER
+// WRITE LIST ITEM ON INPUT: "ENTER"
 input.keypress(function(e){
-    console.log(input.val());
     if (e.keyCode === 13){
         list.append('<li>' + input.val() + '</li>');
         input.val('');
     }
 });
 
+// HIDE AND DISPLAY TEXT INPUT
+plus.on('click',function(){
+    input.toggle(400);
+});
 
-
+// MARK AND UNMARK COMPLETED ITEMS
+listItem.on('click',function(){
+    $(this).toggleClass('line-through');
+});
