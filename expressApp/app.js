ab4c741549195b1872256fe8011c9d0be9938fce
fdevinar <fc_devinar@hotmail.com>
2020-03-26 15:29:15 +0530

    ‘changes’

--- expressApp/app.js
+++ expressApp/app.js
@@ -3,21 +3,43 @@
 let express = require('express');
 let app = express();
 
-// route: index
+// ! INDEX
+// Route: Index
 app.get('/',function(req, res){
-    res.send('INDEX');
+    res.send('Hi there, welcome to my assignment!');
+});
+// ! ANIMALS
+// Route: Pig
+app.get('/speak/pig',function(req, res){
+    res.send('The ping says "Oink!"');
+});
+// Route: Cow
+app.get('/speak/cow',function(req, res){
+    res.send('The cow says "Moo!"');
+});
+// Route: Dog
+app.get('/speak/dog',function(req, res){
+    res.send('The dog says "Woof woof!"');
+});
+// ! REPEAT
+app.get('/repeat/:word/:num',function(req, res){
+
+    let word = req.params.word;
+    let num = Number(req.params.num);
+
+    
+    
+    // let word = req.params.word;
+    // let num = Number(req.params.num);
+    // console.log((word + ' ' )*num);
 });
 
-// route: player
-app.get('/player',function(req, res){
-    res.send('PLAYER');
+// ! NOT FOUND
+// Route: Not Found
+app.get('/*',function(req, res){
+    res.send('Sorry, page not found... What are you doing with your life?');
 });
-
-// route: singer
-app.get('/singer',function(req, res){
-    res.send('SINGER');
-});
-
+// ! INIT SERVER
 // initiate server
 app.listen(3000,function(){
     console.log('Listening on port 3000...');
