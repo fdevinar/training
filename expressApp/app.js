a472b3914b88c4bfa205b78adf21b6064a7a035f
fdevinar <fc_devinar@hotmail.com>
2020-03-27 14:03:37 +0530

    ‘changes’

--- expressApp/app.js
+++ expressApp/app.js
@@ -2,28 +2,53 @@
 
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 app.use(express.static("public"));
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let friends = ['Fabrigol','Guimerlo','Ferita','Duhzera','Rosbife','Denegrido',
+'Markinhos','Danilera','Gabto','GuiCioni']
 
 // Home
 app.get('/',function(req, res){
     res.render('home');
 });
 
-// Football player
-app.get('/football',function(req, res){
-    let team = [
-        {name: 'Grohe', position: 'Goleiro'},
-        {name: 'De Leon', position: 'Zagueiro'},
-        {name: 'Ronaldinho', position: 'Meia'},
-        {name: 'Renato', position: 'Atacante'},
-        {name: 'Jardel', position: 'Centroavante'}
-    ];
-    res.render('football',{team: team});
-})
+// Friends
+app.get('/friends', function(req, res){
+    res.render('friends',{friendList: friends});
+});
+
+// Create Friends
+app.post('/addfriend',function(req, res){
+    let newFriend = req.body.friendName
+    friends.push(newFriend);
+    res.redirect('friends');
+});
+
+
+
+
+
+
 
 // Start server
 app.listen(3000,function(){
     console.log('Server started on port 3000...');
 });
+
+
+
+// Football player
+// app.get('/football',function(req, res){
+//     let team = [
+//         {name: 'Grohe', position: 'Goleiro'},
+//         {name: 'De Leon', position: 'Zagueiro'},
+//         {name: 'Ronaldinho', position: 'Meia'},
+//         {name: 'Renato', position: 'Atacante'},
+//         {name: 'Jardel', position: 'Centroavante'}
+//     ];
+//     res.render('football',{team: team});
+// })
