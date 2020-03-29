fcf028231d946b9633330a862d719513895f78b3
fdevinar <fc_devinar@hotmail.com>
2020-03-30 01:51:49 +0530

    ‘changes’

--- expressApp/APIs/movie/app.js
+++ expressApp/APIs/movie/app.js
@@ -1,23 +1,22 @@
 // * REQUIREMENTS
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); // enables req.body.movieName
 const request = require('request');
 
 // * STANDARDS
 app.use(express.static("public"));
 app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); // enables req.body.movieName
 
 // * ROUTES
-
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.post('/movieLookup', (req, res) => {
     let newMovie = req.body.movieName; // Requested movie
-    let movieApiRequest = `http://www.omdbapi.com/?s=${newMovie}&type=movie&apikey=thewdb` // API Call to Movie DB
+    let movieApiRequest = `http://www.omdbapi.com/?s=${newMovie}&type=movie&apikey=thewdb`;
 
     request(movieApiRequest, (error, response, body) => {
         if (error){
@@ -27,6 +26,10 @@ app.post('/movieLookup', (req, res) => {
             console.log('*** SUCCESS ***')
             console.log(`statusCode: ${response.statusCode}`);
             let movieObject = JSON.parse(body);
+            console.log(movieObject);
+            if (movieObject.Response === 'False'){
+                res.render('notfound');
+            }
             let movies = movieObject.Search;
             res.render('results',{movies:movies});
         }
@@ -35,7 +38,6 @@ app.post('/movieLookup', (req, res) => {
 });
 
 // TODO - FIX WHEN NO MOVIES GET RETURNED
-
 app.post('/movieDetail', (req, res) => {
     let movieID = req.body.Details;
     let movieDetailRequest = `http://www.omdbapi.com/?i=${movieID}&apikey=thewdb`;
@@ -53,19 +55,13 @@ app.post('/movieDetail', (req, res) => {
         }
     });
 
-    // let movieDetailRequest = `http://www.omdbapi.com/?i=${movieID}&apikey=thewdb`
-
-    // res.render('details',{movieDetail:movieDetail});
 } );
 
 // * SERVER INIT
-
 app.listen('3000', () => {
     console.log('Server started listening on port 3000...');
 });
 
-
 // ! API SEARCH
 // General search: http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb 
-
 // Search with Movie ID: http://www.omdbapi.com/?i=tt3896198&apikey=thewdb 
