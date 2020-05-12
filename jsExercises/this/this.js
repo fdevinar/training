690ea1fd0e4bfc0300d3429d1a1120b4233cf64b
fdevinar <fc_devinar@hotmail.com>
2020-05-12 20:29:57 +0530

    ‘changes’

---
+++ jsExercises/this/this.js
@@ -0,0 +1,22 @@
+
+let dog = {
+    name: 'Rex Doggo',
+    bark: function bark(){
+        console.log('Auau, hi I am dog named ' + this.name);
+    },
+    whoIsThis: function whoIsThis(){
+        console.log(this);
+    },
+    friends: {
+        cat: {
+            name: 'Cutie Pie',
+            whoIsThis: function whoIsThis(){
+                console.log(this);
+            }
+        }
+    }
+}
+
+dog.bark(); // Auau, hi I am dog named Rex Doggo
+dog.whoIsThis(); // dog object
+dog.friends.cat.whoIsThis(); // cat object
