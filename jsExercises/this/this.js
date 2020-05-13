c7096c6a6a750c008ed45234c6e2304f3ca22b5a
fdevinar <fc_devinar@hotmail.com>
2020-05-13 18:46:28 +0530

    ‘changes’

--- jsExercises/this/this.js
+++ jsExercises/this/this.js
@@ -1,22 +1,81 @@
+// - IMPLICIT (REFERS TO OBJECT CONTEXT)
 
-let dog = {
-    name: 'Rex Doggo',
-    bark: function bark(){
-        console.log('Auau, hi I am dog named ' + this.name);
-    },
-    whoIsThis: function whoIsThis(){
-        console.log(this); // dog
-    },
-    friends: {
-        cat: {
-            name: 'Cutie Pie',
-            whoIsThis: function whoIsThis(){
-                console.log(this); // cat
-            }
-        }
-    }
-}
+// let dog = {
+// name: 'Rex Doggo',
+// bark: function bark(){
+//     console.log('Auau, hi I am dog named ' + this.name);
+//     // Auau, hi I am dog named Rex Doggo
+// },
+// whoIsThis: function whoIsThis(){
+//     console.log(this); // dog Object
+// },
+// friend: {
+//     whoIsThis: function whoIsThis(){
+//         console.log(this); // friend Object
+//         console.log(this.name); // undefined
+//     }
+// }
+// }
 
-dog.bark(); // Auau, hi I am dog named Rex Doggo
-dog.whoIsThis(); // dog object
-dog.friends.cat.whoIsThis(); // cat object
+// - CALL, APPLY
+
+// let fabri = {
+//     name: 'Fabrigol',
+//     sayHi: function() {
+//         return 'Hi ' + this.name;
+//     },
+//     sum: function(a,b,c,d) {
+//         return this.name + ' likes to rave at ' + (a + b + c + d);
+//     }
+// }
+// let jack = {
+//     name: 'Jack Daniels',
+// }
+
+// fabri.sayHi();
+// //"Hi Fabrigol"
+// fabri.sayHi.call(jack);
+// //"Hi Jack Daniels"
+
+// fabri.sum(1,2,3,4);
+// //"Fabrigol likes to rave at 10"
+// fabri.sum.call(jack,1,2,3,4);
+// //"Jack Daniels likes to rave at 10"
+// fabri.sum.apply(jack,[1,2,3,4]);
+// //"Jack Daniels likes to rave at 10"
+
+// let math = {
+//     name: 'Math Object',
+//     multiply: function(x,y) {
+//         return (this.name + ' returns the result ' + x * y);
+//     }
+// }
+// let XMath = {
+//     name: 'Extra Math Object'
+// }
+
+// math.multiply(3,4)
+// // "Math Object returns the result 12"
+// let calc = math.multiply.bind(XMath);
+// calc(4,5)
+// // "Extra Math Object returns the result 20"
+// calc(6,4)
+// // "Extra Math Object returns the result 24"
+
+// let timer = {
+//     name: 'Timer function',
+//     exec: function() {
+//         setTimeout(function() {
+//             console.log(this.name + ' has been executed right now! on context :' + this);
+//         }.bind(this), 1000);
+//     }
+// }
+
+// // without .bind(this)
+// timer.exec();
+// //  has been executed right now! on context :[object Window]
+
+// // with .bind(this)
+// timer.exec();
+// // Timer function has been executed right now! on context :[object Object]
+
