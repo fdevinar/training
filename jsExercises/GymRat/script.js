405fbbee92653a5e3e04a3c74d031a5f36965bae
fdevinar <fc_devinar@hotmail.com>
2020-05-16 21:49:31 +0530

    ‘changes’

--- jsExercises/GymRat/script.js
+++ jsExercises/GymRat/script.js
@@ -1,15 +1,27 @@
 
 let set = document.getElementById('set');
+let session = document.getElementById('session');
 
-let exercise = {
+let exercise = [{
     name: 'Squat',
     reps: 10,
     sets: 3,
     weight: 20
-}
+}]
 
-function setExercise(exercise) {
-    return `<strong>${exercise.name}</strong>: ${exercise.reps} reps of ${exercise.sets} sets using ${exercise.weight}kg`
-}
 
-set.innerHTML = '<p>' + setExercise(exercise) + '</p>';
+
+let btn = document.querySelector('button');
+btn.addEventListener('click',() => {
+    let form = document.querySelector('form');
+
+    exercise.push({
+        name: form.elements[0].getAttribute('value'),
+        reps: Number(form.elements[1].getAttribute('value')),
+        sets: Number(form.elements[2].getAttribute('value')),
+        weight: Number(form.elements[3].getAttribute('value'))
+    })
+
+    console.log(exercise);
+
+})
