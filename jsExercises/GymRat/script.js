7873a8a925ff4cbb7bcf5436ba6eb587ddd001cc
fdevinar <fc_devinar@hotmail.com>
2020-05-18 06:20:51 +0530

    ‘changes’

--- jsExercises/GymRat/script.js
+++ jsExercises/GymRat/script.js
@@ -12,33 +12,37 @@ let exercises = [{
 displaySession();
 
 btn.addEventListener('click',() => {
-    let form = document.querySelector('form');
-
     let name = document.querySelector('input[id="name"]').value;
     let reps = document.querySelector('input[id="reps"]').value;
     let sets = document.querySelector('input[id="sets"]').value;
     let weight = document.querySelector('input[id="weight"]').value;
 
-    exercises.push({
-        name: name,
-        reps: Number(reps),
-        sets: Number(sets),
-        weight: Number(weight)
-    })
-    console.log('Exercise Added');
+    if (name && reps && sets && weight){
+        exercises.push({
+            name: name,
+            reps: Number(reps),
+            sets: Number(sets),
+            weight: Number(weight) 
+        });
+        console.log('Exercise Added');
+    }
     displaySession();
 });
 
 function displaySession() {
     session.textContent = null;
-    exercises.forEach((exercise) => {
+    exercises.forEach((exercise,index) => {
         console.log(exercise);
-        session.innerHTML += `<ul>
+        session.innerHTML += `
+        <div class="exercise">
+        <h3>${index+1}</h3>
+        <ul>
         <li><strong>${exercise.name}</strong></li>
         <li>Reps: ${exercise.reps}</li>
         <li>Sets: ${exercise.sets}</li>
         <li>${exercise.weight} kg</li>
-        </ul>`
+        </ul>
+        </div>`
     });
 }
 
