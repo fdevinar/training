77a87386401fb7a78371b391c24524bf7681e9ad
fdevinar <fc_devinar@hotmail.com>
2020-06-24 15:22:21 -0300

    ‘changes’

--- Vue/script.js
+++ Vue/script.js
@@ -2,29 +2,28 @@ new Vue({
     el: '#app',
     data: {
         imgLink: 'https://vuejs.org/images/logo.png',
-        counter: 0,
-        x: 0,
-        y: 0
+        effect: 'highlight'
     },
     methods: {
-        //IF USING ARROW FUNC, THIS REFERS TO WINDOW, NOT VUE INSTANCE
-        addCount: function() {
-            this.counter ++;
-        },
-        updateCoordinates: function(event) {
-            this.x = event.clientX;
-            this.y = event.clientY;
+        startEffect: function() {
+            vm = this;
+            setInterval(function(){
+                vm.effect === 'highlight' ? vm.effect = 'shrink' : vm.effect = 'highlight'                
+            },1000);
         }
-    }
+    },
+    watch: {},
+    computed: {}  
 });
 
 //- Vue Notes
 /*
 Directives:
-v-bind:htmlAttribute - > Vue Instance can change HTML attributes
-v-on:event - > add EventListener
+v-bind:htmlAttribute -> Vue Instance can change HTML attributes -> shorthand :src, :href 
+v-on:event -> add EventListener -> shorthand @click, @keydown
 v-once: renders object only once
 v-html: renders raw HTML - use carefully (Cross Site Scripting XSS)
+v-model: 2-way data binding, change data in Vue instance using Html (e.g. input)
 
 Event Modifiers:
 v-on:keydown.enter
@@ -32,5 +31,12 @@ v-on:mousemove.stop
 
 Using Event object:
 v-on:keydown = "myText = $event.target.value"
+
+Comments:
+functions() run everytime the page renders
+Computed properties only run when X value is changed (they run sync)
+Watch can 'listen' to data properties (can run async)
+
+
 
 */
