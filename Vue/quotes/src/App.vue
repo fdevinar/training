ac8360f262cc036fcf7b5ce8df4de0ec050fc980
fdevinar <fc_devinar@hotmail.com>
2020-07-14 19:03:20 -0300

    ‘changes’

--- Vue/quotes/src/App.vue
+++ Vue/quotes/src/App.vue
@@ -1,9 +1,75 @@
 <template>
+
   <div id="app">
-    <progress-bar :progress="quoteList.length"></progress-bar>
-    <add-quote></add-quote>
-    <quote-list :quotes="quoteList"></quote-list>
-    <info></info>
+    <div class="hidden">
+      <progress-bar :progress="quoteList.length"></progress-bar>
+      <add-quote></add-quote>
+      <quote-list :quotes="quoteList"></quote-list>
+      <info></info>
+    </div>
+
+    <h1>Form Exercises</h1>
+    <h2>Workout</h2>
+
+    <div>
+      <label for="trainer">Trainer Name</label>
+      <input type="text" id="trainer" v-model.trim="workoutData.trainer">
+    </div>
+
+    <div>
+      <label for="name">Workout Name</label>
+      <input type="text" id="name" v-model.lazy="workoutData.name">
+    </div>
+
+    <div>
+      <label for="days-monday">Days of Week (Monday)</label>
+      <input type="checkbox" id="days-monday" value="monday" v-model="workoutData.days">
+    </div>
+    <div>
+      <label for="days-wednesdat">Days of Week (Wednesday)</label>
+      <input type="checkbox" id="days-wednesday" value="wednesday" v-model="workoutData.days">
+    </div>
+    <div>
+      <label for="days-friday">Days of Week (Friday)</label>
+      <input type="checkbox" id="days-friday" value="friday" v-model="workoutData.days">
+    </div>
+
+    <div>
+      <label for="intensity">Intensity (0-10)</label>
+      <input type="text" id="intensity" v-model.number.lazy="workoutData.intensity">
+    </div>
+
+    <div>
+      <label for="long">Long Text</label>
+      <textarea rows=3 v-model="long"></textarea>
+    </div>
+
+    <div>
+      <label for="Strength">Strength</label>
+      <input type="radio" value="strength" id="strength" v-model="type">
+    </div>
+
+    <div>
+      <label for="Hypertrophy">Hypertrophy</label>
+      <input type="radio" value="hypertrophy" id="hypertrophy" v-model="type">
+    </div>
+
+  <div>
+    <label for="focus">Focus</label>
+    <select id="focus" name="focus" v-model="focusedArea">
+      <option v-for="area in focus" :key=area>{{ area }}</option>
+    </select>
+  </div>
+
+    <h3>Display Results</h3>
+    <p>Trainer Name: {{ workoutData.trainer }}</p>
+    <p>Workout Name: {{ workoutData.name }}</p>
+    <p>Days of Week: {{ workoutData.days }}</p>
+    <p>Intensity: {{ workoutData.intensity }}</p>
+    <p style="white-space: pre;">Long Text: {{ long }} </p>
+    <p>Type: {{ type }} </p>
+    <p>Focus: {{ focusedArea }} </p>
+
   </div>
 
 
@@ -22,6 +88,17 @@ export default {
   data() {
     return {
       quoteList: [],
+      workoutData: {
+        name: '',
+        trainer: '',
+        days: [],
+        intensity: 5
+      },
+      long: '',
+      type: '',
+      focus: ['Shoulder','Arms','Chest','Legs','Back'],
+      focusedArea: 'Arms'
+
     }
   },
   components: {
@@ -43,6 +120,9 @@ export default {
 </script>
 
 <style>
+.hidden {
+  visibility: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
