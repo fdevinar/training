c14fea4d7119d7431b45d8f88e7f9e673e40bde3
fdevinar <fc_devinar@hotmail.com>
2020-07-15 18:54:51 -0300

    ‘changes’

--- Vue/quotes/src/App.vue
+++ Vue/quotes/src/App.vue
@@ -10,57 +10,64 @@
 
     <h1>Form Exercises</h1>
     <h2>Workout</h2>
+    <div v-if="!submit">
+      <!-- <div>
+        <label for="trainer">Trainer Name</label>
+        <input type="text" id="trainer" v-model.trim="workoutData.trainer">
+      </div>
+
+      <div>
+        <label for="name">Workout Name</label>
+        <input type="text" id="name" v-model.lazy="workoutData.name">
+      </div> -->
+
+    <trainer-workout :data="workoutData"></trainer-workout>
+
+      <div>
+        <label for="days-monday">Days of Week (Monday)</label>
+        <input type="checkbox" id="days-monday" value="monday" v-model="workoutData.days">
+      </div>
+      <div>
+        <label for="days-wednesdat">Days of Week (Wednesday)</label>
+        <input type="checkbox" id="days-wednesday" value="wednesday" v-model="workoutData.days">
+      </div>
+      <div>
+        <label for="days-friday">Days of Week (Friday)</label>
+        <input type="checkbox" id="days-friday" value="friday" v-model="workoutData.days">
+      </div>
+
+      <div>
+        <label for="intensity">Intensity (0-10)</label>
+        <input type="text" id="intensity" v-model.number.lazy="workoutData.intensity">
+      </div>
+
+      <div>
+        <label for="long">Long Text</label>
+        <textarea rows=3 v-model="long"></textarea>
+      </div>
+
+      <div>
+        <label for="Strength">Strength</label>
+        <input type="radio" value="strength" id="strength" v-model="type">
+      </div>
+
+      <div>
+        <label for="Hypertrophy">Hypertrophy</label>
+        <input type="radio" value="hypertrophy" id="hypertrophy" v-model="type">
+      </div>
 
     <div>
-      <label for="trainer">Trainer Name</label>
-      <input type="text" id="trainer" v-model.trim="workoutData.trainer">
+      <label for="focus">Focus</label>
+      <select id="focus" name="focus" v-model="focusedArea">
+        <option v-for="area in focus" :key=area>{{ area }}</option>
+      </select>
     </div>
 
-    <div>
-      <label for="name">Workout Name</label>
-      <input type="text" id="name" v-model.lazy="workoutData.name">
-    </div>
+  <button @click.prevent="submitForm">Submit</button>
 
-    <div>
-      <label for="days-monday">Days of Week (Monday)</label>
-      <input type="checkbox" id="days-monday" value="monday" v-model="workoutData.days">
-    </div>
-    <div>
-      <label for="days-wednesdat">Days of Week (Wednesday)</label>
-      <input type="checkbox" id="days-wednesday" value="wednesday" v-model="workoutData.days">
-    </div>
-    <div>
-      <label for="days-friday">Days of Week (Friday)</label>
-      <input type="checkbox" id="days-friday" value="friday" v-model="workoutData.days">
-    </div>
-
-    <div>
-      <label for="intensity">Intensity (0-10)</label>
-      <input type="text" id="intensity" v-model.number.lazy="workoutData.intensity">
-    </div>
-
-    <div>
-      <label for="long">Long Text</label>
-      <textarea rows=3 v-model="long"></textarea>
-    </div>
-
-    <div>
-      <label for="Strength">Strength</label>
-      <input type="radio" value="strength" id="strength" v-model="type">
-    </div>
-
-    <div>
-      <label for="Hypertrophy">Hypertrophy</label>
-      <input type="radio" value="hypertrophy" id="hypertrophy" v-model="type">
-    </div>
-
-  <div>
-    <label for="focus">Focus</label>
-    <select id="focus" name="focus" v-model="focusedArea">
-      <option v-for="area in focus" :key=area>{{ area }}</option>
-    </select>
   </div>
 
+  <div v-if="submit">
     <h3>Display Results</h3>
     <p>Trainer Name: {{ workoutData.trainer }}</p>
     <p>Workout Name: {{ workoutData.name }}</p>
@@ -69,6 +76,24 @@
     <p style="white-space: pre;">Long Text: {{ long }} </p>
     <p>Type: {{ type }} </p>
     <p>Focus: {{ focusedArea }} </p>
+  </div>
+
+
+<!-- Exercise 1 -->
+<!-- Create a Signup Form where you retrieve the following Information -->
+<!-- Full Name (First Name + Last Name) -->
+<!-- Mail -->
+<!-- Password -->
+<!-- Store Data? Yes/No -->
+
+<!-- Exercise 2 -->
+<!-- Only display the Form if it has NOT been submitted -->
+<!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+<!-- Exercise 3 -->
+<!-- Edit the Example from above and create a custom "Full Name" Control -->
+<!-- which still holds the First Name and Last Name Input Field -->
+
 
   </div>
 
@@ -82,6 +107,7 @@ import ProgressBar from './components/ProgressBar.vue'
 import AddQuote from './components/AddQuote.vue'
 import QuoteList from './components/QuoteList.vue'
 import Info from './components/Info.vue'
+import TrainerWorkout from './components/TrainerWorkout.vue'
 
 export default {
   name: 'App',
@@ -97,15 +123,16 @@ export default {
       long: '',
       type: '',
       focus: ['Shoulder','Arms','Chest','Legs','Back'],
-      focusedArea: 'Arms'
-
+      focusedArea: 'Arms',
+      submit: false
     }
   },
   components: {
     'progress-bar': ProgressBar,
     'add-quote': AddQuote,
     'quote-list': QuoteList,
-    'info': Info
+    'info': Info,
+    'trainer-workout': TrainerWorkout
   },
   created() {
         quoteBus.$on('quoteAdded', (quote) => {
@@ -115,6 +142,11 @@ export default {
                 this.quoteList.push(quote);
             }
         })
+    },
+    methods: {
+      submitForm() {
+        this.submit = true;
+      }
     }
 }
 </script>
---
+++ Vue/quotes/src/components/TrainerWorkout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>
+            <label for="trainer">Trainer Name</label>
+            <input type="text" id="trainer" v-model="data.trainer">
+        </div>
+
+        <div>
+            <label for="name">Workout Name</label>
+            <input type="text" id="name" v-model="data.name">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: Object
+    }
+    
+}
+</script>
