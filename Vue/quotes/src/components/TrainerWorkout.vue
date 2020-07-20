f7b1b8326c0a5b9374bb40b60a1bbac489c7472d
fdevinar <fc_devinar@hotmail.com>
2020-07-19 22:51:32 -0300

    ‘changes’

--- Vue/quotes/src/components/TrainerWorkout.vue
+++ Vue/quotes/src/components/TrainerWorkout.vue
@@ -4,7 +4,6 @@
             <label for="trainer">Trainer Name</label>
             <input type="text" id="trainer" v-model="data.trainer">
         </div>
-
         <div>
             <label for="name">Workout Name</label>
             <input type="text" id="name" v-model="data.name">
