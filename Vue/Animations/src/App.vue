7b290be787befb72abe355f18b9ceeae135dff91
fdevinar <fc_devinar@hotmail.com>
2020-07-31 19:57:55 -0300

    ‘changes’

---
+++ Vue/Animations/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Animation></Animation>
+  </div>
+</template>
+
+<script>
+import Animation from './components/Animation.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Animation
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 80px auto;
+  width: 65%;
+}
+</style>
---
+++ Vue/Animations/src/components/Animation.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <hr>
+    <button class="btn btn-primary" @click="display = !display">Click Me</button>
+    <hr>
+    <transition name="fade">
+      <p class="alert alert-info" v-show="display">Animate this paragraph by clicking the button</p>
+    </transition>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Animations',
+      display: true
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+  /* opacity: 1; */
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+
+
+</style>
